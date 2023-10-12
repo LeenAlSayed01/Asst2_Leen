4a0c0c79761669c022e0a3e825958ebbb0348e3f
LeenAlSayed01 <146116045+LeenAlSayed01@users.noreply.github.com>
2023-10-12 22:58:35 +0300

    Update UniRanking.py

--- UniRanking.py
+++ UniRanking.py
@@ -2,16 +2,11 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 
-
-# Load your CSV data
+# Load the CSV data
 url = "https://raw.githubusercontent.com/LeenAlSayed01/Asst2_Leen/main/World%20University%20Rankings%202023%201.csv"
 df = pd.read_csv(url)
 
-
-
 # Define a placeholder for user input
-import streamlit as st
-
 # Initialize session state to store user input
 if 'user_input' not in st.session_state:
     st.session_state.user_input = ""
@@ -24,35 +19,48 @@ if st.button("Submit"):
     st.session_state.user_input = user_input  # Store user input in session state
     st.title("Hello " + st.session_state.user_input + ", welcome to World University Rankings 2023")
 
+# Sidebar filters
+st.sidebar.title("Filters")
+
+# Filter by Location
+selected_location = st.sidebar.selectbox("Select a location:", df["Location"].unique())
+
+# Filter by No of student
+min_students, max_students = df["No of student"].str.replace(',', '').astype(int).min(), df["No of student"].str.replace(',', '').astype(int).max()
+students_range = st.sidebar.slider("Number of Students", min_students, max_students, (min_students, max_students))
+
+# Filter by Overall Score
+min_score, max_score = df["OverAll Score"].astype(float).min(), df["OverAll Score"].astype(float).max()
+score_range = st.sidebar.slider("Overall Score", min_score, max_score, (min_score, max_score))
+
+# Filter the DataFrame based on user selections
+filtered_df = df[(df["Location"] == selected_location) &
+                 (df["No of student"].str.replace(',', '').astype(int) >= students_range[0]) &
+                 (df["No of student"].str.replace(',', '').astype(int) <= students_range[1]) &
+                 (df["OverAll Score"].astype(float) >= score_range[0]) &
+                 (df["OverAll Score"].astype(float) <= score_range[1])]
+
 # Display the DataFrame and plots if user input is not empty
 if st.checkbox("Click to see the webpage content"):
     st.subheader("The data")
-    st.write(df)
+    st.write(filtered_df)
 
     # Create a scatter plot using Plotly Express
     st.subheader("Scatter Plot: Overall Score vs. International Student Percentage")
-    fig_scatter = px.scatter(df, x="OverAll Score", y="International Student", 
-                            hover_name="Name of University", title="Scatter Plot: Overall Score vs. International Student Percentage")
-   
+    fig_scatter = px.scatter(filtered_df, x="OverAll Score", y="International Student", 
+                             hover_name="Name of University", title="Scatter Plot: Overall Score vs. International Student Percentage")
     st.plotly_chart(fig_scatter)  
-     
-
-    #fig 2
-    fig2 = px.line(df, x="No of student", y="No of student per staff", color="Name of University",
-                    title="Changes in University Rankings Over Time")
+    
+    # Figure 2
+    fig2 = px.line(filtered_df, x="No of student", y="No of student per staff", color="Name of University",
+                   title="Changes in University Rankings Over Time")
     st.plotly_chart(fig2)
 
-    #fig3
-    
-    
-    
-    
-    
-    select_university = st.selectbox("Select a university:", df["Name of University"].unique())
-    filtered_df = df[df["Name of University"] == select_university]
-   
+    # Figure 3
+    select_university = st.selectbox("Select a university:", filtered_df["Name of University"].unique())
+    university_df = filtered_df[filtered_df["Name of University"] == select_university]
     fig3 = px.choropleth(
-        filtered_df,
+        university_df,
         locations="Location",
         locationmode="country names",
         color="Name of University",
@@ -61,11 +69,3 @@ if st.checkbox("Click to see the webpage content"):
         projection="natural earth",
     )
     st.plotly_chart(fig3)
-
-import pandas as pd
-import streamlit as st
-import plotly.express as px
-
-# Load your CSV data
-url = "https://raw.githubusercontent.com/LeenAlSayed01/Asst2_Leen/main/World%20University%20Rankings%202023%201.csv"
-df = pd.read_csv(url)
